322a464e412005ee547044e00de42b916eb3478d
abinand <abinandsathyan532gmail.com>
2023-01-20 20:30:28 +0530

    coupon date change

--- controllers/usercontroler.js
+++ controllers/usercontroler.js
@@ -658,6 +658,7 @@ const  getcheckout=async(req,res)=>{
   console.log(check);
 
 
+  
   if(check==false)
   {
     req.flash("prolimit",products+" out of stock")
@@ -894,7 +895,7 @@ const verifycoupon= async(req,res)=>{
     console.log(expireDate,couponType,cutOff,maxRedeemAmount);
     
   if(generateCount>=1){
-    if(expireDate>nowDate){
+    if(expireDate<nowDate){
     if(couponType=="Amount"){
     if(total<minCartAmount)
        {
